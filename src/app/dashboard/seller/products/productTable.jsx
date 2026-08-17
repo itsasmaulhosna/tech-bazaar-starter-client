dcd59a4fc0eb11fc30ae136bdb65ec33ea42256a
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-08-17 23:59:21 +0600

    implemented producttable

--- src/app/dashboard/seller/products/productTable.jsx
+++ src/app/dashboard/seller/products/productTable.jsx
@@ -1,4 +1,5 @@
 import {Table} from "@heroui/react";
+import Image from "next/image";
 
 export function ProductTable({products}) {
     console.log(products)
@@ -8,18 +9,20 @@ export function ProductTable({products}) {
         <Table.Content aria-label="Team members" className="min-w-[600px]">
           <Table.Header>
             <Table.Column isRowHeader>Image</Table.Column>
-            <Table.Column>Price</Table.Column>
-            <Table.Column>Quantity</Table.Column>
             <Table.Column>Title</Table.Column>
-            
+            <Table.Column>Quantity</Table.Column>
+            <Table.Column>Price</Table.Column>
           </Table.Header>
           <Table.Body>
             {
                 products.map(product=><Table.Row key={product._id}>
-              <Table.Cell>{product.title}</Table.Cell>
-              <Table.Cell>{product.price}</Table.Cell>
+                    <Table.Cell>
+                        <Image height={50} width={50} className="object-cover h-10 w-10" src={product.image || "/placeholder.png"} alt={product.title}/>
+                    </Table.Cell>
+              <Table.Cell>{product.name}</Table.Cell>
               <Table.Cell>{product.quantity}</Table.Cell>
-              <Table.Cell></Table.Cell>
+              <Table.Cell>{product.price}</Table.Cell>
+              
             </Table.Row>)
             }
           </Table.Body>
